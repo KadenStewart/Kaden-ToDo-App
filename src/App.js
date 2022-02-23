a359552519c887daf3d296443ffe7baf76ef1810
KadenStewart <97703176+KadenStewart@users.noreply.github.com>
2022-02-23 14:58:26 -0600

    CoreHTML

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="todo-container">
       <h1>To-Do List!</h1>
+      <form>
+        <input tpye="text" />
+        <button type="submit">Add Todo!</button>
+      </form>
+
+      <div className="todo">
+        {/* todo item text */}
+        <div className="todo-text">
+          <input type="checkbox" />
+          <div>Eat Pizza</div>
+        </div>
+
+        {/* Buttons */}
+        <div className="todo-actions">
+          <button>Update</button>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
