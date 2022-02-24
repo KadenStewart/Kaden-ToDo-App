853063ce8bee9ff2ce64ef33a71b7fce5f188625
KadenStewart <97703176+KadenStewart@users.noreply.github.com>
2022-02-24 15:17:06 -0600

    Form Populates To-Do List.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // state
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [editingText, setEditingText] = useState("");
+
+  // functions
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+    setTodoList([...todoList].concat(newTodo));
+    setTodo("");
+  }
+
   return (
     <div className="todo-container">
       <h1>To-Do List!</h1>
-      <form>
-        <input tpye="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          tpye="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
         <button type="submit">Add Todo!</button>
       </form>
 
+      {todoList.map((todo) => (
+        <div key={todo.id}>
+          <div>{todo.text}</div>
+        </div>
+      ))}
+
+      {/* 
       <div className="todo">
-        {/* todo item text */}
         <div className="todo-text">
           <input type="checkbox" />
           <div>Eat Pizza</div>
         </div>
 
-        {/* Buttons */}
         <div className="todo-actions">
           <button>Update</button>
           <button>Edit</button>
           <button>Delete</button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
